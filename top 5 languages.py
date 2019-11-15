fcd3b8655171e0f302512ffd6714af479e0f95e0
cglawson89 <52048840+cglawson89@users.noreply.github.com>
2019-11-14 21:40:16 -0500

    Add files via upload

---
+++ top 5 languages.py
@@ -0,0 +1,9 @@
+tweets_by_lang = tweets['lang'].value_counts()
+
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Languages', fontsize=15)
+ax.set_ylabel('Number of tweets' , fontsize=15)
+ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
---
+++ tweets by country.py
@@ -0,0 +1,9 @@
+tweets_by_country = tweets['country'].value_counts()
+
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Countries', fontsize=15)
+ax.set_ylabel('Number of tweets' , fontsize=15)
+ax.set_title('Top 5 countries', fontsize=15, fontweight='bold')
+tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
